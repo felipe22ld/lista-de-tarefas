6830ff8bad9b07daa270edb8e3455e8065cc386b
Felipe Lima <felipedias@hotmail.com.br>
2022-04-17 17:11:16 -0300

    frist commit

---
+++ src/components/AddArea/styled.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+
+export const Container = styled.div`
+    border:1px solid #CCC;
+    border-radius: 15px;
+    padding: 10px;
+    display:flex;
+    align-items:center;
+    margin:20px 0;
+
+    .image{
+        margin-right: 15px;
+    }
+
+    input{
+        border:0px;
+        background:transparent;
+        outline:0;
+        color:#FFF;
+        font-size: 18px;
+        flex:1;
+    }
+
+`
